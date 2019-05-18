f2aaedba76ee161b085a3c4328e4a69423709dcd
Petja Valkama <petja.valkama@futurice.com>
2019-05-18 11:33:12 +0300

    Initial commit

---
+++ src/MainView.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import './App.css';
+
+class MainView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+
+  sendData = () => {
+    fetch("/", {
+      method: "post",
+      body: {
+        number: this.state.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="MainView">
+        <textarea className="TextInput" value={this.state.value} onChange={this.onChange}></textarea>
+        <button className="Button" onClick={this.sendData}>subcrsibe</button>
+      </div>
+    );
+  }
+}
+
+export default MainView;
